929449ad9c244721d945a677598181463dddad1c
ashishmishra112 <am3913570@gmail.com>
2026-05-13 12:54:54 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-// import DisableInspect from "@/app/components/DisableInspect";
+import DisableInspect from "@/app/components/DisableInspect";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -61,7 +61,7 @@ export default function RootLayout({
       <body 
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      {/* <DisableInspect />  */}
+      <DisableInspect /> 
         {children}
         
       </body>
